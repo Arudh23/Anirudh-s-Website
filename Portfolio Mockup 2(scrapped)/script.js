fcb6615f84336e2bb923ede8bcfff5ec03d734d3
AnirudhA <rudhani180@gmail.com>
2023-01-17 00:07:12 +0530

    Portfolio Final Mockup
    Almost done with the personal website, content addition is pending.

---
+++ Portfolio Mockup 2(scrapped)/script.js
@@ -0,0 +1,5 @@
+// JavaScript source code
+const header = document.querySelector("header");
+window.addEventListener("scroll", function () {
+    header.classList.toggle("sticky", window.screenY > 0);
+});
